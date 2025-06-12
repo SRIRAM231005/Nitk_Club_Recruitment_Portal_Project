2a4175372f8ef491661e10b8e69c98ecbf6c5bcc
B Sriram <136240742+SRIRAM231005@users.noreply.github.com>
2025-06-12 22:56:16 +0530

    Add files via upload

--- ClubRecBackend/middleware/auth.js
+++ ClubRecBackend/middleware/auth.js
@@ -3,19 +3,21 @@ require('dotenv').config();
 
 // Middleware to authenticate the token
 function authenticateToken(req, res, next) {
-    const token = req.header('Authorization')?.split(' ')[1]; // Bearer <token>
+    const token = req.header('Authorization')?.split(' ')[1]; 
 
     if (!token) {
         return res.status(403).json({ message: 'No token provided, access denied' });
     }
 
-    // Verify the token
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
             return res.status(403).json({ message: 'Invalid or expired token' });
         }
 
-        req.user = decoded; // Save the decoded user info
+        req.user = decoded;
+        if(req.user.userRole != "Club Convener"){
+            return res.status(403).json({ message: 'Invalid or expired token' });
+        }
         next();
     });
 }
