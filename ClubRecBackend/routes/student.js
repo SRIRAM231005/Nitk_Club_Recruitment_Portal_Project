7d183c353ba9d7d19e37719f3816b2646aecb622
B Sriram <136240742+SRIRAM231005@users.noreply.github.com>
2025-06-12 22:58:08 +0530

    Add files via upload

--- ClubRecBackend/routes/student.js
+++ ClubRecBackend/routes/student.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const {GetAnnouncement,GetRegForm,SetStudentData,SetSigPreference,Calender} = require('../controllers/announcement_control');
+const {GetAnnouncement,GetRegForm,SetStudentData,SetStudentAssingmentData,SetSigPreference,Calender} = require('../controllers/announcement_control');
 const announcementRouter = express.Router();
 
 announcementRouter.route('/ann').get(GetAnnouncement);
 announcementRouter.route('/getreg').get(GetRegForm);
 announcementRouter.route('/setdata').patch(SetStudentData);
+announcementRouter.route('/setassingmentdata').patch(SetStudentAssingmentData);
 announcementRouter.route('/sigPref').post(SetSigPreference);
 announcementRouter.route('/calender').get(Calender);
 
